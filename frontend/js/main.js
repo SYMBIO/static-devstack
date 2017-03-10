bf4b5230932c75f0ded0b068149944c4a822fff1
Pavel Šuraba <pavel.suraba@symbiodigital.com>
2017-03-10 11:01:25 +0100

    clean up

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -6,20 +6,4 @@ if (module.hot) module.hot.accept();
 
 document.addEventListener('DOMContentLoaded', () => {
     svg4everybody();
-
-    /**
-     * async/await
-     * DELETE this code
-     */
-    const mockFetch = () => {
-        return new Promise(resolve => {
-            setTimeout(() => resolve('READY'), 2000);
-        });
-    };
-
-    async function fetchUser() {
-        const user = await mockFetch();
-        cl(user);
-    }
-    fetchUser();
 });
