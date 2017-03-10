2fc38a3355137a015e57c8f92b8b16e8d2ad2f46
Pavel Šuraba <pavel.suraba@symbiodigital.com>
2017-03-10 11:03:44 +0100

    async

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -6,4 +6,26 @@ if (module.hot) module.hot.accept();
 
 document.addEventListener('DOMContentLoaded', () => {
     svg4everybody();
+
+    /**
+     * Async demonstration
+     */
+    const mockFetch = (delay) => {
+        return new Promise(resolve => {
+            setTimeout(() => resolve('READY'), delay);
+        });
+    };
+
+    /* async/await */
+    async function fetchUser() {
+        const user = await mockFetch(2000);
+        cl('1 - Await ', user);
+    }
+    fetchUser();
+
+    /* Promise usage */
+    mockFetch(1500)
+        .then(data => cl('2 - Promise ', data));
+
+    cl('3 - Synchronous');
 });
